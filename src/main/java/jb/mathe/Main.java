3fffe019164484a335a3e3865239dca253e3ff37
jabar <ja.baranec@gmail.com>
2023-09-30 13:00:58 +0200

    Kalkulation der Polynomdivision erstellt und ein weiteres Beispiel

--- src/main/java/jb/mathe/Main.java
+++ src/main/java/jb/mathe/Main.java
@@ -3,12 +3,22 @@ package jb.mathe;
 public class Main {
     public static void main(String[] args) {
         //Simulate an easy example of the polynomdivision
-        Funktion dividend = new Funktion(3);
-        dividend.setKoeffizienten(new int[] {1, 2, 3, 4});
-        Funktion divisor = new Funktion(1);
-        divisor.setKoeffizienten(new int[] {1, 2});
+        Funktion dividend = new Funktion(5);
+        dividend.setKoeffizienten(new int[] {-4, 6, 1, 5,-4,-6});
+        Funktion divisor = new Funktion(3);
+        divisor.setKoeffizienten(new int[] {1, -1, 0,-2});
+
         Polynomdivision polynomdivision = new Polynomdivision(dividend, divisor);
-//        polynomdivision.calculate();
         polynomdivision.print();
+
+        //Beispiel 2 (x^7-x^6+3x^5+3x^4-6x^3) / (x^3-x^2)
+        Funktion dividend2 = new Funktion(7);
+        dividend2.setKoeffizienten(new int[] {0,0,0,-6,3,3,-1,1});
+        Funktion divisor2 = new Funktion(3);
+        divisor2.setKoeffizienten(new int[] {0,0,-1,1});
+
+        Polynomdivision polynomdivision2 = new Polynomdivision(dividend2, divisor2);
+        polynomdivision2.print();
+
     }
 }
--- src/main/java/jb/mathe/Polynomdivision.java
+++ src/main/java/jb/mathe/Polynomdivision.java
@@ -1,25 +1,57 @@
 package jb.mathe;
 
 public class Polynomdivision {
-    private Funktion dividend;
-    private Funktion divisor;
-    private Funktion quotient;
+    private final Funktion dividend;
+    private final Funktion divisor;
+    private final Funktion quotient;
 
     public Polynomdivision(Funktion dividend, Funktion divisor) {
         this.dividend = dividend;
         this.divisor = divisor;
         this.quotient = new Funktion(dividend.getGrad() - divisor.getGrad());
+        calculate();
     }
 
     //Calculate the quotient
     private void calculate() {
+        // Lokale Variablen, die nur in dieser Methode verwendet werden.
+        // Damit wird sichergestellt, dass die Variablen nicht überschrieben werden
+        int[] dividendKoeffizienten = this.dividend.getKoeffizienten().clone();
+        int[] divisorKoeffizienten = this.divisor.getKoeffizienten().clone();
+        int[] quotientKoeffizienten = this.quotient.getKoeffizienten().clone();
+        int tempQuotient;
+        int tempDividendGrad = this.dividend.getGrad();
+        int tempQuotientGrad = this.quotient.getGrad();
+        int[] tempDividendKoeffizienten; //Zwischenspeicher für die Koeffizienten des Dividenden
 
+        // Kopie der Koeffizienten des Dividenden, damit dividendKoeffizienten nicht überschrieben werden
+        tempDividendKoeffizienten = this.dividend.getKoeffizienten().clone();
+
+
+        quotientKoeffizienten[this.quotient.getGrad()] = dividendKoeffizienten[this.dividend.getGrad()] / divisorKoeffizienten[this.divisor.getGrad()];
+        tempQuotient = quotientKoeffizienten[this.quotient.getGrad()]; // Quotient der ausgerechnet wurde
+
+        // Achtung! der tempDividendGrad wird hier verändert, die anderen Variablen nicht!
+        calculateTempDividendKoeffizienten(tempDividendKoeffizienten, tempDividendGrad, tempQuotient, divisorKoeffizienten);
+
+        for (int i = tempQuotientGrad; i > 0; i--) {
+            tempQuotientGrad -= 1;
+            tempDividendGrad -= 1;
+            quotientKoeffizienten[tempQuotientGrad] = tempDividendKoeffizienten[tempDividendGrad] / divisorKoeffizienten[this.divisor.getGrad()];
+            tempQuotient = quotientKoeffizienten[tempQuotientGrad];
+
+            // Achtung! der tempDividendGrad wird hier verändert, die anderen Variablen nicht!
+            calculateTempDividendKoeffizienten(tempDividendKoeffizienten, tempDividendGrad, tempQuotient, divisorKoeffizienten);
+        }
+
+        this.quotient.setKoeffizienten(quotientKoeffizienten);
     }
 
-    //Calculate the qutioent and return it
-    public Funktion getQuotient() {
-//        this.calculate();
-        return this.quotient;
+    private void calculateTempDividendKoeffizienten(int[] tempKoeffizienten, int tempDividendGrad, int tempQuotient, int[] divisorKoeffizienten) {
+        for (int i = this.divisor.getGrad(); i >= 0; i--) {
+            tempKoeffizienten[tempDividendGrad] -= tempQuotient * divisorKoeffizienten[i];
+            tempDividendGrad--;
+        }
     }
 
     //Print the dividend, divisor and quotient
