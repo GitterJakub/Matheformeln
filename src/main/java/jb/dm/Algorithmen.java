c2ffd4b6c4996b33ef5814fa2fc8a1f85d51e822
baranjak <ja.baranec@gmail.com>
2024-01-17 10:31:06 +0100

    euklid

--- src/main/java/jb/dm/Algorithmen.java
+++ src/main/java/jb/dm/Algorithmen.java
@@ -1,2 +1,31 @@
-package jb.dm;public class Algorithmen {
+package jb.dm;
+
+public class Algorithmen {
+    public Algorithmen() {
+    }
+
+    public static int ggT(int a, int b) {
+        System.out.println("ggt(" + a + "," + b + ")");
+        if (b == 0) return a;
+        if (a==0) return b;
+        if (a<b) return ggT(b%a, a);
+        return ggT(b, a%b);
+    }
+
+    public static void teilenMitRest(int dividend, int divisor) {
+        System.out.println("teilenMitRest(" + dividend + "," + divisor + ")");
+        if (divisor <= 0){
+            throw new IllegalArgumentException("b muss größer als 0 sein!");
+        }
+        int quotient = dividend/divisor;
+        int rest = berechneRest(dividend, divisor);
+        System.out.println("Quotient: " + quotient + " Rest: " + rest);
+    }
+
+    private static int berechneRest(int rest, int divisor){
+        System.out.println("berechneRest(" + rest + "," + divisor + ")");
+        if (rest < divisor) return rest;
+        return berechneRest(rest-divisor, divisor);
+    }
+
 }
--- src/main/java/jb/dm/Main.java
+++ src/main/java/jb/dm/Main.java
@@ -1,2 +1,10 @@
-package jb.dm;public class Main {
+package jb.dm;
+
+public class Main {
+    public static void main(String[] args) {
+        int test = Algorithmen.ggT(274867353,996897039);
+        System.out.println(test + " ist der ggT von 27 und 96");
+        Algorithmen.teilenMitRest(274867353,4556);
+    }
+
 }
